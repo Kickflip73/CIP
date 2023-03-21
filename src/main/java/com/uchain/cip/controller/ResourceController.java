c6eedebe6338f83053c20d0e3f7cb0f4c7435232
kickflip <3065242502@qq.com>
2023-03-21 21:55:27 +0800

    重构基本数据类

---
+++ src/main/java/com/uchain/cip/controller/ResourceController.java
@@ -0,0 +1,9 @@
+package com.uchain.cip.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resources")
+public class ResourceController {
+}
