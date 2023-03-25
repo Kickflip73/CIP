beab092d4882fe1ed40741e8547120b572f6e3ea
赵小超 <1954509371@qq.com>
2023-03-25 15:55:26 +0800

    Merge branch 'master' of https://github.com/Kickflip73/CIP
    Signed-off-by: 赵小超 <1954509371@qq.com>

--- src/main/java/com/uchain/cip/pojo/Star.java
+++ src/main/java/com/uchain/cip/pojo/Star.java
@@ -7,17 +7,17 @@ import lombok.Data;
 * 收藏的实体类
 * */
 @Data
-@ApiModel("收藏信息实体类")
+//@ApiModel("收藏信息实体类")
 public class Star {
-    @ApiModelProperty("收藏id")
+//    @ApiModelProperty("收藏id")
     private int id;
-    @ApiModelProperty("用户id")
+//    @ApiModelProperty("用户id")
     private int user_id;
-    @ApiModelProperty("帖子id")
+//    @ApiModelProperty("帖子id")
     private  int thing_id;
-    @ApiModelProperty("收藏时间")
+//    @ApiModelProperty("收藏时间")
     private int star_date_time;
-    @ApiModelProperty("帖子类型id")
+//    @ApiModelProperty("帖子类型id")
     private int thing_type;
 
 
