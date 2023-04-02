006a5e4f62710a59b0508a94084d11eae14d805e
kickflip <3065242502@qq.com>
2023-04-02 12:17:55 +0800

    增加复杂邮件方式

--- src/main/java/com/uchain/cip/tools/EmailUtil.java
+++ src/main/java/com/uchain/cip/tools/EmailUtil.java
@@ -3,7 +3,13 @@ package com.uchain.cip.tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.List;
 
 /**
  * 邮件发送工具类
@@ -11,8 +17,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailUtil {
     @Autowired
-    private JavaMailSender mailSender;
+    private JavaMailSender javaMailSender;
 
+    /**
+     * 发送简单邮件
+     * */
     public void sendSimpleMailMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         //设置发件人
@@ -24,6 +33,29 @@ public class EmailUtil {
         //内容
         message.setText(text);
 
-        mailSender.send(message);
+        javaMailSender.send(message);
+    }
+
+    /**
+     * 发送复杂邮件
+     * */
+    public void sendMultipartMailMessage(String to, String subject, String text, List<File> fileList) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(Constant.FROM_EMAIL);
+            helper.setText(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+
+            for (File file : fileList) {
+                helper.addAttachment(file.getName(), file);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        javaMailSender.send(message);
     }
 }
--- src/test/java/com/uchain/cip/service/SendEmailTest.java
+++ src/test/java/com/uchain/cip/service/SendEmailTest.java
@@ -5,13 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.util.ArrayList;
+
 @SpringBootTest
 public class SendEmailTest {
     @Autowired
     EmailUtil emailUtil;
 
     @Test
-    public void testSendSimpleMessageEmail() {
+    public void testSendSimpleMessage() {
 //        emailUtil.sendSimpleMailMessage("3065242502@qq.com", "Hello", "测试内容");
+    }
+
+    @Test
+    public void testSendMultipartMailMessage() {
+        ArrayList<File> fileList = new ArrayList<>();
+
+        File file = new File("classpath:/application.yml");
+        fileList.add(file);
+        emailUtil.sendMultipartMailMessage("3065242502@qq.com", "Hello", "测试内容", fileList);
     }
 }
